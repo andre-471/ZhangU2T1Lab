6fcd3b347aabe44950b94a88a7b1774dc0d9fd71
andre <73719598+andre-471@users.noreply.github.com>
2023-09-25 19:10:32 -0400

    idk

--- src/Student.java
+++ src/Student.java
@@ -1,19 +1,23 @@
 public class Student {
-    String name;
-    double gpa;
-    double height;
+    private String name;
+    private double gpa;
+    private double height;
     public Student(String n, double g, double h) {
         name = n;
         gpa = g;
-        height = h;
+        height = h; // in inches
     }
     public void info() {
         System.out.println("This person's name is " + name);
+        System.out.println("This person's GPA is " + gpa);
+        System.out.println("This person's height is " + (int) (height / 12) + " ft " + height % 12 + " in");
     }
 
     public void ifFailing() {
         if (gpa >= 65) {
-            System.out.println("not fail");
+            System.out.println(name + " is not currently failing");
+        } else {
+            System.out.println(name + " is currently failing");
         }
     }
 }
--- src/StudentRunner.java
+++ src/StudentRunner.java
@@ -1,5 +1,11 @@
 public class StudentRunner {
     public static void main(String[] args) {
+        Student student1 = new Student("Marc", 62, 69);
+        student1.info();
+        student1.ifFailing();
 
+        Student student2 = new Student("Joe", 92, 72);
+        student2.info();
+        student2.ifFailing();
     }
 }
